3e2a4200600c36e5fab3421b4bc7725fa1a50134
abel555 <taqui555@gmail.com>
2018-09-01 17:14:40 -0400

    pantalla inicial con iconos aun sin posicionar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,11 +6,15 @@ import { StatusBar } from '@ionic-native/status-bar';
 
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
+import { ListaPage } from '../pages/lista/lista';
+
+
 
 @NgModule({
   declarations: [
     MyApp,
-    HomePage
+    HomePage,
+    ListaPage
   ],
   imports: [
     BrowserModule,
@@ -19,7 +23,8 @@ import { HomePage } from '../pages/home/home';
   bootstrap: [IonicApp],
   entryComponents: [
     MyApp,
-    HomePage
+    HomePage,
+    ListaPage
   ],
   providers: [
     StatusBar,
--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { ListaPage } from '../lista/lista';
 
 @Component({
   selector: 'page-home',
@@ -9,6 +10,12 @@ export class HomePage {
 
   constructor(public navCtrl: NavController) {
 
+  }
+  pushPage(){
+    // push another page onto the navigation stack
+    // causing the nav controller to transition to the new page
+    // optional data can also be passed to the pushed page.
+    this.navCtrl.push(ListaPage);
   }
 
 }
