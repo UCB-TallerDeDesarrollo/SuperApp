7350485cf731b797fc983842869c93569bf63daf
lopolafa <brandonburgoafigueroa@gmail.com>
2018-09-01 23:38:25 -0400

    añadiendo atributos product y color

--- src/pages/word/word.ts
+++ src/pages/word/word.ts
@@ -1,7 +1,8 @@
-import { ProductManager } from './productManager';
+import { ProductManager } from './Managers/ProductManager';
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
 import { NavParams } from 'ionic-angular';
+import { ColorsManager } from './Managers/ColorsManager';
 
 @Component({
   selector: 'page-word',
@@ -10,7 +11,9 @@ import { NavParams } from 'ionic-angular';
 
 export class WordPage {
   product:string;
+  color:string;
   constructor(public navCtrl: NavController) {
     this.product=ProductManager.get_product();
+    this.color=ColorsManager.get_color_style();
   }
 }
