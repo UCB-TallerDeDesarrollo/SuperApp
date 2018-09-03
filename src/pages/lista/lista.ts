81a052aa3594ec4d416a20e873f2802d930d807c
kevin cardenas <kevincardenasmiranda9@gmail.com>
2018-09-03 17:49:30 -0400

    Agregando pagina para ver la lista de productos

--- src/pages/lista/lista.ts
+++ src/pages/lista/lista.ts
@@ -1,14 +1,20 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
 
+import { ProductsPage } from '../products/products';
+
 @Component({
   selector: 'page-lista',
   templateUrl: 'lista.html'
 })
 export class ListaPage {
-
+  
   constructor(public navCtrl: NavController) {
 
+  }
+
+  pushProducts(){
+    this.navCtrl.push(ProductsPage);
   }
 
 }
