6b9efb8b030f2ce5fa01385e90fa16827a86c19d
kevin camacho <kevinacho_z900@hotmail.com>
2018-09-03 15:24:09 -0400

    Moviendo codigo a una nueva rama partiendo de develop lista

--- src/pages/lista/lista.ts
+++ src/pages/lista/lista.ts
@@ -1,14 +1,15 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
-
+import { DataBaseService } from '../../providers/database-service/database-service';
 @Component({
   selector: 'page-lista',
   templateUrl: 'lista.html'
 })
 export class ListaPage {
 
+  public products:any;
   constructor(public navCtrl: NavController) {
-
+    this.products=DataBaseService.getProducts();
   }
-
+ 
 }
