db5bf8730b99c9ad8b139c18f32687b303183aaf
SamuelUCB <val97samuel@hotmail.com>
2018-09-05 15:49:59 -0300

    I1.6 | Changed var to let in ArrayManager from word component

--- src/pages/word/Managers/ArrayManager.ts
+++ src/pages/word/Managers/ArrayManager.ts
@@ -1,12 +1,12 @@
 export class ArrayManager {
 
     static get_random_element(array:string[]) {
-        var index=this.random_int(0, array.length-1);
+        let index = this.random_int(0, array.length - 1);
         return array[index];   
     }
 
     static random_int(min, max){
         return Math.floor(Math.random() * (max - min + 1)) + min;
     }
-    
+
 }
