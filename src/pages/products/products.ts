9b17a6794c127d29d0fa0b372c2eb34b60936514
kevin cardenas <kevincardenasmiranda9@gmail.com>
2018-09-08 12:58:56 -0400

    removiendo import sin utilizar

--- src/pages/products/products.ts
+++ src/pages/products/products.ts
@@ -1,6 +1,5 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
-import { FakeProducts } from '../../providers/FakeService/FakeProducts';
 import { FakeListProducts } from '../../providers/FakeService/FakeListProducts';
 
 /**
