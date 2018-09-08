5fbacaa21e12f0f474327507c531b11bb06bbcf7
kevin cardenas <kevincardenasmiranda9@gmail.com>
2018-09-07 21:49:06 -0400

    añadir a carrito y eliminar la imagen

--- src/pages/lista/lista.ts
+++ src/pages/lista/lista.ts
@@ -14,31 +14,23 @@ export class ListaPage implements OnInit, OnDestroy, AfterViewInit {
   
   actualSelectedElement:any;
   actualSelectedContainer:any;
-  recentlyMove:boolean;
   count: number;
   products: Array<{id: number, title: string, image: string}> = [];
 
   constructor(public navCtrl: NavController, private dragulaService: DragulaService) {
     this.products = FakeProducts.getProducts();
-    this.recentlyMove = false;
     this.count = 0;
   }
 
   ngOnInit() {
-    console.log("ngOnInit()");
     this.dragulaService.createGroup("PRODUCT", {
       revertOnSpill: false,
       moves: (element, container, handle) => {
-        console.log("class--> "+container.children[0].classList);
-        console.log("length--> "+container.children.length);
-        return !(container.children.length > 0 && container.children[0].classList.contains('no-move'));
+        console.log(container.classList);
+        return !(container.children[0].classList.contains('no-move')) || !container.classList.contains('ignore-item');
       },
       accepts: (element, target, source, sibling) => {
-        console.log(target.classList.contains('objetive-container'));
         if(!target.classList.contains('objetive-container')) {
-          return false;
-        }
-        if(target.children.length > 0) {
           return false;
         }
         return true;
@@ -53,22 +45,8 @@ export class ListaPage implements OnInit, OnDestroy, AfterViewInit {
   ngAfterViewInit() { 
     console.log("ngAfterViewInit()");
 
-    this.dragulaService.drag('PRODUCT').subscribe(({ name, el, source }) => {
-      console.log("SUBSCRIBE 1--> "+source);
-      this.actualSelectedContainer = source;
-    });
-
     this.dragulaService.drop("PRODUCT").subscribe(({ el, target, source, sibling }) => {
-      console.log("SUBSCRIBE 2--> "+el);
-    });
-
-    this.dragulaService.dragend('PRODUCT').subscribe(({ name, el }) => {
-      console.log("SUBSCRIBE 3--> "+el.children);
-    });
-
-    this.dragulaService.cloned('PRODUCT').subscribe(({ clone, original, cloneType }) => {
-      console.log("SUBSCRIBE 4--> "+clone);
-      this.actualSelectedElement = clone;
+      el.remove();
     });
 
   }
@@ -76,21 +54,4 @@ export class ListaPage implements OnInit, OnDestroy, AfterViewInit {
   pushProducts(){
     this.navCtrl.push(ProductsPage);
   }
-
-  offset(el) {
-    let rect = el.getBoundingClientRect(),
-    scrollLeft = window.pageXOffset || document.documentElement.scrollLeft,
-    scrollTop = window.pageYOffset || document.documentElement.scrollTop;
-    return { top: rect.top + scrollTop, left: rect.left + scrollLeft }
-  }
-
-  showEndView() {
-    console.log(this.count);
-    ++this.count;
-    //if(this.count >= this.letter_response.length) {
-      console.log('GANASTE');
-    //}
-  }
-
-
 }
