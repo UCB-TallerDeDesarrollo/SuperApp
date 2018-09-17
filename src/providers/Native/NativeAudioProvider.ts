ad4417c00e0d2892cc1e942e826be48a00364f83
SamuelUCB <val97samuel@hotmail.com>
2018-09-17 04:35:17 -0300

    I1.12 | Modificados condicionales para dispositivos reales

--- src/providers/Native/NativeAudioProvider.ts
+++ src/providers/Native/NativeAudioProvider.ts
@@ -3,16 +3,28 @@ import { AudioProvider } from '../../shared/providers/AudioProvider';
 
 export class NativeAudioProvider implements AudioProvider {
     
-    private correctLetterSound = new Audio('../../assets/sounds/correctLetterSound.mp3');
+    private correctLetterSound: HTMLAudioElement;
 
     public constructor(private nativeAudio: NativeAudio) {
-        //nativeAudio.preloadSimple('correctLetterSound', '../../assets/sounds/correctLetterSound.mp3').then(onSuccess, onError);
-        this.correctLetterSound.volume = 0.2;
+        if(this.isRealDevice()) {
+            nativeAudio.preloadSimple('correctLetterSound', '../../assets/sounds/correctLetterSound.mp3');
+        }
+        else {
+            this.correctLetterSound = new Audio('../../assets/sounds/correctLetterSound.mp3');
+            this.correctLetterSound.volume = 0.2;
+        }
     }
 
     public playCorrectLetterSound(): void {
-        //this.nativeAudio.play('correctLetterSound');
-        this.correctLetterSound.play();
+        if(this.isRealDevice()) {
+            this.nativeAudio.play('correctLetterSound');
+        }
+        else {
+            this.correctLetterSound.play();
+        }
     }
 
+    private isRealDevice(): boolean {
+        return document.URL.indexOf('http') !== 0;
+    }
 }
