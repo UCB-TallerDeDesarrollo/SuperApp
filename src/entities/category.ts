45fd9d81545b3d148ae62a6cfd8e796eefc6feb8
kevin cardenas <kevincardenasmiranda9@gmail.com>
2018-09-18 21:38:19 -0400

    Agregando entidad category

---
+++ src/entities/category.ts
@@ -0,0 +1,11 @@
+import {Entity, PrimaryGeneratedColumn, Column} from "typeorm";
+
+@Entity('category')
+export class Category {
+
+    @PrimaryGeneratedColumn()
+    id: number;
+
+    @Column()
+    name: string;
+}
