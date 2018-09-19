4c6595b67ce28d9597fd6ed5539ab2e5c3581901
kevin cardenas <kevincardenasmiranda9@gmail.com>
2018-09-18 22:18:08 -0400

    Agregando conneccion a la base de datos

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,6 +4,9 @@ import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
 
 import { HomePage } from '../pages/home/home';
+import { createConnection } from 'typeorm';
+import { Product } from '../entities/product';
+import { Category } from '../entities/category';
 
 @Component({
   templateUrl: 'app.html'
@@ -13,11 +16,39 @@ export class MyApp {
   
 
   constructor(platform: Platform, statusBar: StatusBar, splashScreen: SplashScreen) {
-    platform.ready().then(() => {
+    platform.ready().then(async () => {
       statusBar.styleDefault();
       this.hideSplashScreen(splashScreen);
+
+      if (platform.is('cordova')) {
+        await createConnection({
+          type: 'cordova',
+          database: 'test',
+          location: 'default',
+          logging: ['error', 'query', 'schema'],
+          synchronize: true,
+          entities: [
+            Category,
+            Product
+          ]
+        }); 
+      } else {
+        await createConnection({
+          type: 'sqljs',
+          autoSave: true,
+          location: 'browser',
+          logging: ['error', 'query', 'schema'],
+          synchronize: true,
+          entities: [
+            Category,
+            Product
+          ]
+        });
+      }
+
     });
   }
+
   hideSplashScreen(splashScreen) {
     if (splashScreen) {
       setTimeout(() => {
