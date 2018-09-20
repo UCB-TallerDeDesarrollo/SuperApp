f81a62acc7056bafac8db558bf67479160dbd5c1
kevin cardenas <kevincardenasmiranda9@gmail.com>
2018-09-20 05:50:38 -0400

    Agregando provider para product_level

--- src/providers/product-level/product-level.ts
+++ src/providers/product-level/product-level.ts
@@ -1,6 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { getRepository, Repository } from 'typeorm';
+import { ProductLevel } from '../../entities/productLevel';
 
 /*
   Generated class for the ProductLevelProvider provider.
@@ -14,8 +15,19 @@ export class ProductLevelProvider {
   productLevelRepository: any;
 
   constructor() {
-    this.productLevelRepository = getRepository('product_level') ;
+    this.productLevelRepository = getRepository('product_level') as Repository<ProductLevel>;
     console.log('Hello ProductLevelProvider Provider');
+  }
+
+  async saveProductLevel(productLevel: ProductLevel) {
+    await this.productLevelRepository.save(productLevel);
+  }
+
+  async getProductLevelByLevelId(level_id_: number) {
+    let productlevel = this.productLevelRepository.createQueryBuilder('product_level')
+                                                  .where('level_id = :level_id', { level_id: level_id_ })
+                                                  .getOne();
+    return productlevel;                                                  
   }
 
 }
