aec8f24a65a34faf193d1d429eff0ad1c76a44a2
jorgechavezrnd <jorgechavezrnd@gmail.com>
2018-09-22 19:04:08 -0400

    Prueba para get MessyWord terminada

--- src/shared/models/SortWordGame.model.spec.ts
+++ src/shared/models/SortWordGame.model.spec.ts
@@ -1,5 +1,7 @@
 import { SortWordGame } from './sortWordGame.model';
 import { Product } from './product.model';
+import { ColorProvider } from '../providers/ColorProvider';
+import { ArrayColorProvider } from '../../providers/Array/ArrayColorProvider';
 
 describe("Test SortWordGame model", function() {
     let product: Product;
@@ -36,5 +38,19 @@ describe("Test SortWordGame model", function() {
 
     it('must return the title of product', function() {
         expect(sortWordGame.ResponseWord).toBe(product.Title);
+    });
+
+    it('must return messy letters of response word', function() {
+        let colorProvider: ColorProvider;
+        colorProvider = new ArrayColorProvider();
+
+        let response: any = [];
+        for (let letter of sortWordGame.ResponseWord) {
+            response[letter] = colorProvider.getRandomColor();
+        }
+        
+        sortWordGame.buildLetters(response);
+
+        expect(sortWordGame.MessyWord).not.toBe(sortWordGame.SortedWord);
     });
 });
