10c37934e8cc7ef575aff2f2c5ed091af2e0c0c8
jorgechavezrnd <jorgechavezrnd@gmail.com>
2018-09-22 16:00:56 -0400

    Metodos beforeEach y afterEach creados

--- src/shared/models/SortWordGame.model.spec.ts
+++ src/shared/models/SortWordGame.model.spec.ts
@@ -1,5 +1,17 @@
 import { SortWordGame } from './sortWordGame.model';
+import { Product } from './product.model';
 
 describe("Test SortWordGame model", function() {
+    let product: Product;
+    let sortWordGame: SortWordGame;
 
+    beforeEach(function() {
+        product = Product.createProduct(1, 'title1', 'image1', 2);
+        sortWordGame = new SortWordGame(product);
+    });
+
+    afterEach(function() {
+        product = null;
+        sortWordGame = null;
+    });
 });
