1073d9486f64c7f37497e7df84e9ea57e11a22cb
jorgechavezrnd <jorgechavezrnd@gmail.com>
2018-09-22 11:39:55 -0400

    Metodos beforeEach y afterEach terminados

--- src/shared/models/Product.model.spec.ts
+++ src/shared/models/Product.model.spec.ts
@@ -1,5 +1,13 @@
 import { Product } from './product.model';
 
 describe("Test Product model", function() {
+    let product: Product;
 
+    beforeEach(function() {
+        product = new Product();
+    });
+
+    afterEach(function() {
+        product = null;
+    });
 });
