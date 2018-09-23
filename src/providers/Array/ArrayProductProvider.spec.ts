b9cf972b8b3e9aef77cbe56559ca0242aaa27d73
abel555 <taqui555@gmail.com>
2018-09-23 16:51:21 -0400

    test para getProductOf actual level en verde

--- src/providers/Array/ArrayProductProvider.spec.ts
+++ src/providers/Array/ArrayProductProvider.spec.ts
@@ -1,8 +1,15 @@
+import { Product } from './../../shared/models/product.model';
 import { ArrayProductProvider } from './ArrayProductProvider';
 describe("Test ArrayProductProvider static functions", function() {
     let arrayProductProvider: ArrayProductProvider;
+    const path_images = '../../assets/imgs/Products/'
     beforeEach(function() {
         arrayProductProvider = new ArrayProductProvider();
+    });
+    it("must return the correspondent product", function() {
+        let product: Product;
+        product = Product.createProduct(1, 'AJO', path_images+'ajo.jpg',1);
+        expect(arrayProductProvider.getProductOfActualLevel()).toEqual(product)
     });
     it("must return the actual level", function() {
        expect(arrayProductProvider.getActualLevel()).toBe(1)
@@ -21,7 +28,7 @@ describe("Test ArrayProductProvider static functions", function() {
     it("must return quantity of products", function() {
         expect(arrayProductProvider.getQuantityOfProducts()).toBe(60)
     });
-    
+   
     
 
 });
