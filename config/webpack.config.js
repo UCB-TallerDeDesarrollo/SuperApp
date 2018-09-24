413056c914e6d3ac7058dfdbb010710868ae9dcb
SamuelUCB <val97samuel@hotmail.com>
2018-09-24 06:49:39 -0300

    Borrando webpack package

--- config/webpack.config.js
+++ config/webpack.config.js
@@ -1,4 +1,4 @@
-var path = require('path');
+/*var path = require('path');
 var webpack = require('webpack');
 var ionicWebpackFactory = require(process.env.IONIC_WEBPACK_FACTORY);
 
@@ -124,3 +124,37 @@ module.exports = {
     dev: devConfig,
     prod: prodConfig
 }
+*/
+
+var webpack = require('webpack');
+var ionicWebpackFactory = require(process.env.IONIC_WEBPACK_FACTORY);
+
+var ModuleConcatPlugin = require('webpack/lib/optimize/ModuleConcatenationPlugin');
+var PurifyPlugin = require('@angular-devkit/build-optimizer').PurifyPlugin;
+
+var useDefaultConfig = require('@ionic/app-scripts/config/webpack.config.js');
+
+useDefaultConfig.dev.plugins = [
+  ionicWebpackFactory.getIonicEnvironmentPlugin(),
+  ionicWebpackFactory.getCommonChunksPlugin(),
+  new webpack.NormalModuleReplacementPlugin(/typeorm$/, function (result) {
+    result.request = result.request.replace(/typeorm/, "typeorm/browser");
+  }),
+  new webpack.ProvidePlugin({
+    'window.SQL': 'sql.js/js/sql.js'
+  })
+]
+
+useDefaultConfig.prod.plugins = [
+  ionicWebpackFactory.getIonicEnvironmentPlugin(),
+  ionicWebpackFactory.getCommonChunksPlugin(),
+  new ModuleConcatPlugin(),
+  new PurifyPlugin(),
+  new webpack.NormalModuleReplacementPlugin(/typeorm$/, function (result) {
+    result.request = result.request.replace(/typeorm/, "typeorm/browser");
+  })
+]
+
+module.exports = function () {
+  return useDefaultConfig;
+};
