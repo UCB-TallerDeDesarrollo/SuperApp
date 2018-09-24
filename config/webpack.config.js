00bd3d91792eb3d444b3ec1a0eed4fd937bc407c
SamuelUCB <val97samuel@hotmail.com>
2018-09-24 03:35:04 -0300

    Agregando formato a webpack.config.js

--- config/webpack.config.js
+++ config/webpack.config.js
@@ -5,132 +5,122 @@ var ionicWebpackFactory = require(process.env.IONIC_WEBPACK_FACTORY);
 var PurifyPlugin = require('@angular-devkit/build-optimizer').PurifyPlugin;
 
 var optimizedProdLoaders = [
-  {
-    test: /\.json$/,
-    loader: 'json-loader'
-  },
-  {
-    test: /\.js$/,
-    loader: [
-      {
-        loader: process.env.IONIC_CACHE_LOADER
-      },
-
-      {
-        loader: '@angular-devkit/build-optimizer/webpack-loader',
-        options: {
-          sourceMap: true
+    {
+        test: /\.json$/,
+        loader: 'json-loader'
+    },
+    {
+        test: /\.js$/,
+        loader: [
+        {
+            loader: process.env.IONIC_CACHE_LOADER
+        },
+        {
+            loader: '@angular-devkit/build-optimizer/webpack-loader',
+            options: {
+                sourceMap: true
+            }
         }
-      },
-    ]
-  },
-  {
-    test: /\.ts$/,
-    loader: [
-      {
-        loader: process.env.IONIC_CACHE_LOADER
-      },
-
-      {
-        loader: '@angular-devkit/build-optimizer/webpack-loader',
-        options: {
-          sourceMap: true
+        ]
+    },
+    {
+        test: /\.ts$/,
+        loader: [
+        {
+            loader: process.env.IONIC_CACHE_LOADER
+        },
+        {
+            loader: '@angular-devkit/build-optimizer/webpack-loader',
+            options: {
+                sourceMap: true
+            }
+        },
+        {
+            loader: process.env.IONIC_WEBPACK_LOADER
         }
-      },
-
-      {
-        loader: process.env.IONIC_WEBPACK_LOADER
-      }
-    ]
-  }
+        ]
+    }
 ];
 
 function getProdLoaders() {
-  if (process.env.IONIC_OPTIMIZE_JS === 'true') {
-    return optimizedProdLoaders;
-  }
-  return devConfig.module.loaders;
+    if (process.env.IONIC_OPTIMIZE_JS === 'true') {
+        return optimizedProdLoaders;
+    }
+    return devConfig.module.loaders;
 }
 
 var devConfig = {
-  entry: process.env.IONIC_APP_ENTRY_POINT,
-  output: {
-    path: '{{BUILD}}',
-    publicPath: './build/',
-    filename: '[name].js',
-    devtoolModuleFilenameTemplate: ionicWebpackFactory.getSourceMapperFunction(),
-  },
-  devtool: process.env.IONIC_SOURCE_MAP_TYPE,
+    entry: process.env.IONIC_APP_ENTRY_POINT,
+    output: {
+        path: '{{BUILD}}',
+        publicPath: './build/',
+        filename: '[name].js',
+        devtoolModuleFilenameTemplate: ionicWebpackFactory.getSourceMapperFunction(),
+    },
+    devtool: process.env.IONIC_SOURCE_MAP_TYPE,
+    resolve: {
+        extensions: ['.ts', '.js', '.json'],
+        modules: [path.resolve('node_modules')]
+    },
+    module: {
+        loaders: [
+        {
+            test: /\.json$/,
+            loader: 'json-loader'
+        },
+        {
+            test: /\.ts$/,
+            loader: process.env.IONIC_WEBPACK_LOADER
+        }
+        ]
+    },
+    plugins: [
+        ionicWebpackFactory.getIonicEnvironmentPlugin(),
+        ionicWebpackFactory.getCommonChunksPlugin(),
+        new PurifyPlugin()
+    ],
 
-  resolve: {
-    extensions: ['.ts', '.js', '.json'],
-    modules: [path.resolve('node_modules')]
-  },
-
-  module: {
-    loaders: [
-      {
-        test: /\.json$/,
-        loader: 'json-loader'
-      },
-      {
-        test: /\.ts$/,
-        loader: process.env.IONIC_WEBPACK_LOADER
-      }
-    ]
-  },
-
-  plugins: [
-    ionicWebpackFactory.getIonicEnvironmentPlugin(),
-    ionicWebpackFactory.getCommonChunksPlugin(),
-    new PurifyPlugin()
-  ],
-
-  // Some libraries import Node modules but don't use them in the browser.
-  // Tell Webpack to provide empty mocks for them so importing them works.
-  node: {
-    fs: 'empty',
-    net: 'empty',
-    tls: 'empty'
-  }
+    // Some libraries import Node modules but don't use them in the browser.
+    // Tell Webpack to provide empty mocks for them so importing them works.
+    node: {
+        fs: 'empty',
+        net: 'empty',
+        tls: 'empty'
+    }
 };
 
 var prodConfig = {
-  entry: process.env.IONIC_APP_ENTRY_POINT,
-  output: {
-    path: '{{BUILD}}',
-    publicPath: './build/',
-    filename: '[name].js',
-    devtoolModuleFilenameTemplate: ionicWebpackFactory.getSourceMapperFunction(),
-  },
-  devtool: process.env.IONIC_SOURCE_MAP_TYPE,
+    entry: process.env.IONIC_APP_ENTRY_POINT,
+    output: {
+        path: '{{BUILD}}',
+        publicPath: './build/',
+        filename: '[name].js',
+        devtoolModuleFilenameTemplate: ionicWebpackFactory.getSourceMapperFunction(),
+    },
+    devtool: process.env.IONIC_SOURCE_MAP_TYPE,
+    resolve: {
+        extensions: ['.ts', '.js', '.json'],
+        modules: [path.resolve('node_modules')]
+    },
+    module: {
+        loaders: getProdLoaders()
+    },
+    plugins: [
+        ionicWebpackFactory.getIonicEnvironmentPlugin(),
+        ionicWebpackFactory.getCommonChunksPlugin(),
+        new PurifyPlugin()
+    ],
 
-  resolve: {
-    extensions: ['.ts', '.js', '.json'],
-    modules: [path.resolve('node_modules')]
-  },
-
-  module: {
-    loaders: getProdLoaders()
-  },
-
-  plugins: [
-    ionicWebpackFactory.getIonicEnvironmentPlugin(),
-    ionicWebpackFactory.getCommonChunksPlugin(),
-    new PurifyPlugin()
-  ],
-
-  // Some libraries import Node modules but don't use them in the browser.
-  // Tell Webpack to provide empty mocks for them so importing them works.
-  node: {
-    fs: 'empty',
-    net: 'empty',
-    tls: 'empty'
-  }
+    // Some libraries import Node modules but don't use them in the browser.
+    // Tell Webpack to provide empty mocks for them so importing them works.
+    node: {
+        fs: 'empty',
+        net: 'empty',
+        tls: 'empty'
+    }
 };
 
-
 module.exports = {
-  dev: devConfig,
-  prod: prodConfig
+    dev: devConfig,
+    prod: prodConfig
 }
