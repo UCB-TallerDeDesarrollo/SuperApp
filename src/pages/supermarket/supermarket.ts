46eb2c84bc6eb17f3c23c048bd33a9ca9cf6f232
kevin camacho <kevinacho_z900@hotmail.com>
2018-10-07 17:50:24 -0400

    Añadiendo logica de preparacion para el juego del super mercado

--- src/pages/supermarket/supermarket.ts
+++ src/pages/supermarket/supermarket.ts
@@ -12,12 +12,27 @@ import {SuperMarketGame} from '../../shared/models/SupermarketGame';
 })
 export class SupermarketPage implements OnInit, AfterViewInit{
    
+  game : SuperMarketGame;
+  products: Array<{ id: number, title: string, image: string, state: boolean, categoryId: number}> = [];
+  productsToBuy: any=[]; 
+
   constructor(
     public navController: NavController, 
     public navParams: NavParams,
     public productsProvider:   ProductsProvider,
     public categoryProvider: CategoryProvider
   ) { 
+    this.prepareGame();
+  }
+   
+  async prepareGame(){
+    this.products = await this.productsProvider.getProducts(); 
+    this.game = new SuperMarketGame(this.products);
+    this.game.buildProducts(8,6);  
+    this.productsToBuy=this.game.ProductsToBuy;  
+  } 
+ 
+
   ionViewDidLoad() {  
   }
 
