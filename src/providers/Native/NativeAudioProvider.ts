656680543ca67a662bbad4a5fc2717321be7fcb9
christianfp <christian_flores995@hotmail.com>
2018-10-07 15:15:27 -0400

    errores corregidos al silenciar en dispositivo

--- src/providers/Native/NativeAudioProvider.ts
+++ src/providers/Native/NativeAudioProvider.ts
@@ -6,7 +6,7 @@ export class NativeAudioProvider implements AudioProvider {
     
     private correctLetterSound : HTMLAudioElement;
     private levelComplete      : HTMLAudioElement;
-    public static isMuted      : boolean = false;
+    public static muted     : boolean = false;
 
     public constructor(private nativeAudio: NativeAudio, private platform: Platform) {
         if(this.isRealDevice()) {
@@ -29,34 +29,38 @@ export class NativeAudioProvider implements AudioProvider {
     }
    
     public isMuted(){
-        return NativeAudioProvider.isMuted;
+        return NativeAudioProvider.muted;
     }
     public leveCompleteMute(){
        this.levelComplete.muted=true;
        this.levelComplete.volume=0.0;
+       this.nativeAudio.setVolumeForComplexAsset('levelComplete', 0.0);
     }
     public leveCompleteUnMute(){
         this.levelComplete.muted=false;
         this.levelComplete.volume=0.8;
+        this.nativeAudio.setVolumeForComplexAsset('levelComplete', 0.8);
      }
      public correctLetterSoundUnmute(){
         this.correctLetterSound.muted=false;
         this.correctLetterSound.volume = 0.8;
+        this.nativeAudio.setVolumeForComplexAsset('correctLetterSound', 0.8);
      }
      public correctLetterSoundMute(){
         this.correctLetterSound.muted=true;
         this.correctLetterSound.volume = 0.0;
+        this.nativeAudio.setVolumeForComplexAsset('correctLetterSound', 0.0);
      }
     changeState():void{
-        if (NativeAudioProvider.isMuted==true) {
+        if (NativeAudioProvider.muted==true) {
             this.correctLetterSoundUnmute();
             this.leveCompleteUnMute();
-            NativeAudioProvider.isMuted=false
+            NativeAudioProvider.muted=false;
         }    
         else {   
             this.correctLetterSoundMute();
             this.leveCompleteMute();
-            NativeAudioProvider.isMuted=true;
+            NativeAudioProvider.muted=true;
         }
     }
 
