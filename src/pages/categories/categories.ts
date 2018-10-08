80b6e98f35f9581043a0a9124afb010f00326ff9
Kevin Cardenas <kevincardenasmiranda9@gmail.com>
2018-10-08 05:51:03 -0400

    Creando vista basica para la vista de categorias

--- src/pages/categories/categories.ts
+++ src/pages/categories/categories.ts
@@ -1,5 +1,8 @@
 import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { IonicPage, NavController, NavParams, ModalController } from 'ionic-angular';
+import { Category } from '../../entities/category';
+import { CategoryProvider } from '../../providers/category/category';
+import { ProductProvider } from '../../shared/providers/ProductProvider';
 
 @IonicPage()
 @Component({
@@ -8,11 +11,34 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
 })
 export class CategoriesPage {
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  categories: Array<Category>
+
+  constructor(public navCtrl: NavController, 
+              public navParams: NavParams,
+              public categoryProvider: CategoryProvider,
+              public productProvider: ProductProvider,
+              public modalController: ModalController) {
   }
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad CategoriesPage');
   }
+ 
+  ionViewWillEnter() {
+    this.reloadCategories();  
+  }
 
+  reloadCategories() {
+    this.categoryProvider.getCategories()
+    .then(categories => {
+      this.categories = categories;
+      console.log(JSON.stringify(categories));
+    }).catch(error => {
+      console.error(error);
+    });
+  }
+  
+  openCategoryModal() {
+    console.log("OpenCategoryModal");
+  }
 }
