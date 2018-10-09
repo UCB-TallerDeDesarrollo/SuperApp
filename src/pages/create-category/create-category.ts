3d9185f44989cac75b90f89246d6fd8de1cb392e
Kevin Cardenas <kevincardenasmiranda9@gmail.com>
2018-10-09 17:02:11 -0400

    Formulario para agregar categoria

--- src/pages/create-category/create-category.ts
+++ src/pages/create-category/create-category.ts
@@ -1,5 +1,8 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams, ViewController } from 'ionic-angular';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Category } from '../../entities/category';
+import { CategoryProvider } from '../../providers/category/category';
 
 
 @IonicPage()
@@ -9,9 +12,17 @@ import { IonicPage, NavController, NavParams, ViewController } from 'ionic-angul
 })
 export class CreateCategoryPage {
 
+  category = new Category;
+  categoryForm: FormGroup;
+
   constructor(public navCtrl: NavController, 
               public navParams: NavParams,
-              public viewController: ViewController) {
+              public viewController: ViewController,
+              public formBuilder: FormBuilder,
+              public categoryProvider: CategoryProvider) {
+    this.categoryForm = this.formBuilder.group({
+      name: ['', Validators.required]
+    });
   }
 
   ionViewDidLoad() {
@@ -22,4 +33,7 @@ export class CreateCategoryPage {
     this.viewController.dismiss();
   }
 
+  saveCategoryForm() {
+    console.log("saveCategoryForm");
+  }
 }
