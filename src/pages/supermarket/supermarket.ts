34ea5369ff58d2c1509b89ce411693a238aca79e
joseAnagua <anah2o.jlaq@gmail.com>
2018-10-11 19:29:16 -0400

    modificando el codigo de la pagina supermarket

--- src/pages/supermarket/supermarket.ts
+++ src/pages/supermarket/supermarket.ts
@@ -1,29 +1,34 @@
 import { CategoryProvider } from './../../providers/category/category';
-import { Component, OnInit, AfterViewInit } from '@angular/core'; 
+import { Component, OnInit, AfterViewInit, OnDestroy } from '@angular/core'; 
 import { IonicPage, NavController, NavParams } from 'ionic-angular'; 
  
 import { ProductsProvider } from '../../providers/product/product'; 
 import {SuperMarketGame} from '../../shared/models/SupermarketGame'; 
 import { AudioProvider } from '../../shared/providers/AudioProvider';
+import { SupermarketDragDropProvider } from '../../shared/providers/SupermarketDragDropProvider';
 @IonicPage()
 @Component({
   selector: 'page-supermarket',
   templateUrl: 'supermarket.html',
 })
-export class SupermarketPage implements OnInit, AfterViewInit{
+export class SupermarketPage implements OnInit, AfterViewInit, OnDestroy {
    
   game : SuperMarketGame;
   products: Array<{ id: number, title: string, image: string, state: boolean, categoryId: number}> = [];
   productsToBuy: any=[]; 
   productsToPlay: any[];
   imageSound: String;
+  public selectorName: string;
+
   constructor(
     public navController: NavController, 
     public navParams: NavParams,
     public productsProvider:   ProductsProvider,
     public categoryProvider: CategoryProvider,
-    private audioProvider: AudioProvider
-  ) { 
+    private audioProvider: AudioProvider,
+    private dragDropProvider: SupermarketDragDropProvider
+  ) {
+    this.selectorName = 'PRODUCT-' + Math.random();
     this.prepareGame();
     this.changeSoundIcon(); 
   }
@@ -31,7 +36,7 @@ export class SupermarketPage implements OnInit, AfterViewInit{
   async prepareGame(){
     this.products = await this.productsProvider.getProducts(); 
     this.game = new SuperMarketGame(this.products);
-    this.game.buildProducts(8,6);  
+    this.game.buildProducts(8,6);
     this.productsToBuy=this.game.ProductsToBuy; 
     this.productsToPlay=this.game.ProductsToPlay; 
   } 
@@ -53,10 +58,16 @@ private changeSoundIcon(){
     this.changeSoundIcon(); 
   }
 
-  ngOnInit(): void { 
+  ngOnInit(): void {
+    this.dragDropProvider.initialize(this.selectorName);
   }
 
-  ngAfterViewInit(): void { 
+  ngAfterViewInit(): void {
+    this.dragDropProvider.startEvents(this.selectorName, this);
+  }
+
+  ngOnDestroy(): void {
+    this.dragDropProvider.finalize(this.selectorName);
   }
 
   popPage(){
