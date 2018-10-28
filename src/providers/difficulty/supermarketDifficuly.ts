296c95a69d03b0186008c380fd81b9b7e36d20eb
kevin camacho <kevinacho_z900@hotmail.com>
2018-10-28 14:18:09 -0400

    Añadiendo provider para dificultad del juego del super

---
+++ src/providers/difficulty/supermarketDifficuly.ts
@@ -0,0 +1,10 @@
+import { Injectable } from '@angular/core'; 
+import { SupermarketDifficulty as DifficultyEntity } from './../../entities/supermarketDifficulty';
+import { Difficulty as DifficultyModel } from '../../shared/models/Difficulty.model';
+import { getRepository, Repository } from 'typeorm';
+import { DifficultyProvider } from '../../shared/providers/DifficultyProvider';
+
+@Injectable()
+export class TypeormSupermarketDifficultyProvider implements DifficultyProvider {
+    
+}
