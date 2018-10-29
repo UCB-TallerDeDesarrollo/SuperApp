29128effbe703a92c3f31d82516d2f7bf60f6e70
Kevin Cardenas <kevincardenasmiranda9@gmail.com>
2018-10-29 00:20:40 -0400

    agregando metodo para obentern productos de una lista

--- src/providers/product-list/product-list.ts
+++ src/providers/product-list/product-list.ts
@@ -23,4 +23,16 @@ export class ProductListProvider {
     return result;
   }
 
+  async getProductListByListId(list_id: number): Promise<Array<ProductList>> {
+    let result: Array<ProductList>;
+    try {
+      result = this.productListRepository.createQueryBuilder()
+                                          .where("list_id = :listId", { listId: list_id})
+                                          .getMany();
+    } catch (error) {
+      console.error(error);
+      result = null;
+    }
+    return result;
+  }
 }
