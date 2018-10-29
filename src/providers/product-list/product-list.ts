fbf2b138aaf74d39909fc3ace8248d5fbfa1e894
Kevin Cardenas <kevincardenasmiranda9@gmail.com>
2018-10-29 00:17:13 -0400

    agregando metodo para guardar productList

--- src/providers/product-list/product-list.ts
+++ src/providers/product-list/product-list.ts
@@ -1,10 +1,26 @@
+import { getRepository, Repository } from 'typeorm';
 import { Injectable } from '@angular/core';
+import { ProductList } from '../../entities/productList';
 
 @Injectable()
 export class ProductListProvider {
 
+  productListRepository: any;
+
   constructor() {
-    console.log('Hello ProductListProvider Provider');
+    this.productListRepository = getRepository('product_list') as Repository<ProductList>;
+  }
+
+  async saveProductList(productList: ProductList): Promise<Boolean> {
+    let result: Boolean;
+    try {
+      await this.productListRepository.save(productList);
+      result = true;
+    } catch (error) {
+      console.error(error);
+      result = false;
+    }
+    return result;
   }
 
 }
