c26f33bed113557a818659e285867c978356061c
Kevin Cardenas <kevincardenasmiranda9@gmail.com>
2018-11-01 22:21:23 -0400

    ahora se pude crear categorias

--- src/pages/create-list/create-list.ts
+++ src/pages/create-list/create-list.ts
@@ -1,8 +1,12 @@
+import { UserProvider } from './../../providers/user/user';
+import { ListaPage } from './../lista/lista';
 import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 import { List } from './../../entities/list';
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
 import { ListProvider } from '../../providers/list/list';
+import { User } from '../../entities/user';
+import { LoginStatus } from '../../providers/login/LoginStatus';
 
 
 @IonicPage()
@@ -12,24 +16,37 @@ import { ListProvider } from '../../providers/list/list';
 })
 export class CreateListPage {
 
+  user: User;
   list = new List;
   listForm: FormGroup;
 
   constructor(public navCtrl: NavController,
               public navParams: NavParams,
               public formBuilder: FormBuilder,
-              public listProvider: ListProvider) {
+              public listProvider: ListProvider,
+              public userProvider :UserProvider) {
     this.listForm = this.formBuilder.group({
       name: ['', Validators.required]
     });
+    this.userProvider.getUserByUsername(LoginStatus.username)
+    .then(user => {
+      this.user = user;
+      this.list.user_id = this.user.id;
+    }).catch(error => {
+      console.error(error);
+    })
   }
 
   async saveListForm() {
     this.listProvider.saveList(this.list)
     .then(response => {
-      if(response) console.error("Inconsistent list information");
+      if(response) this.afterSaveList();
     }).catch(error => {
       console.error(error);
     })
+  }
+
+  afterSaveList() {
+    this.navCtrl.pop();
   }
 }
