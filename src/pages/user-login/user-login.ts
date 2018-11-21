9ec9fa282135f88af0c70ffb9b162c839dda923a
lopolafa <brandonburgoafigueroa@gmail.com>
2018-11-21 01:17:53 -0400

    user login con select

--- src/pages/user-login/user-login.ts
+++ src/pages/user-login/user-login.ts
@@ -3,26 +3,29 @@ import {NavController, NavParams, ToastController } from 'ionic-angular';
 import { UserProvider } from '../../providers/user/user';
 import { Login } from '../../providers/login/Login';
 import { LoginOptionsPage } from '../login-options/login-options';
+import { User } from '../../entities/user';
 
 @Component({
   selector: 'page-user-login',
   templateUrl: 'user-login.html',
 })
 export class UserLoginPage{
-  public username: string;
   private loginOptions:LoginOptionsPage;
+  public users:User[];
   constructor(public navCtrl: NavController, public navParams: NavParams, public userProvider:UserProvider, private toastCtrl: ToastController, public loginCtrl:Login) {
     this.loginOptions=navParams.get("loginOptions");
     
   }
-
-  public async login()
+  async ionViewDidLoad(){
+    this.users= await this.userProvider.getAllUsers();
+  }
+  public async login(username:string)
   {
-    if (await this.loginCtrl.login(this.username))
+    if (await this.loginCtrl.login(username))
     {
      await this.loginCtrl.loadingGameData();
       let toast=this.toastCtrl.create({
-        message:"Bienvenido "+this.username,
+        message:"Bienvenido "+username,
         duration: 3000,
         position: 'bottom'
       });
@@ -32,7 +35,7 @@ export class UserLoginPage{
     }
     else{
       let toast=this.toastCtrl.create({
-        message:"El usuario "+this.username+" no existe",
+        message:"El usuario "+username+" no existe",
         duration: 3000,
         position: 'bottom'
       })
