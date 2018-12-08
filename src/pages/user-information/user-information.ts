87a91cea61925886596871cf2ab22d69988672d5
lopolafa <brandonburgoafigueroa@gmail.com>
2018-12-08 19:59:28 -0400

    dark theme en informacion de usuario

--- src/pages/user-information/user-information.ts
+++ src/pages/user-information/user-information.ts
@@ -1,14 +1,25 @@
 import { LoginStatus } from './../../providers/login/LoginStatus';
 import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { IonicPage, NavController, NavParams, NavPopAnchor } from 'ionic-angular';
 import { UserProgress } from '../../entities/userProgress';
 @Component({
   selector: 'page-user-information',
   templateUrl: 'user-information.html',
 })
 export class UserInformationPage {
+  public darkTheme:boolean=false;
   constructor(public navCtrl: NavController, public navParams: NavParams) {
-  
+    this.setupTheme();
+  }
+  setupTheme(): void {
+    let dark=this.navParams.get("dark");
+    if (dark)
+    {
+      this.darkTheme=true;
+    }
+    else{
+      this.darkTheme=false;
+    }
   }
   get username(){
     return LoginStatus.username;
